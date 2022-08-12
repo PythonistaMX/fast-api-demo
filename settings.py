52f1a67e1576ca5192073de9071ab9857d532eb1
Jose Luis Chiquete <josech@gmail.com>
2022-08-11 21:50:30 -0500

    220811b

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ DB_PASSWORD = getenv('DB_PASSWORD')
 if  getenv('GAE_ENV') == 'standard':
     SQLALCHEMY_DATABASE_URL = 'sqlite:///db.sqlite3'
 else:
+    '''
     SQLALCHEMY_DATABASE_URL = 'sqlite:///db.sqlite3'
     '''
     SQLALCHEMY_DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@localhost/pythonista'
-    '''
